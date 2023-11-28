9ef14cc29b830d47b57aedd9bae71c898f6ab2e1
devasadshaikh <officialshaikhasad@gmail.com>
2023-11-28 23:35:00 +0500

    Update page.tsx
    h

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -2,7 +2,7 @@
 export default function Contact (){
     return(
         <div>
-            <h1  className=" text-2xl text-cyan-800  mt-5 ml-5 bg-rose-200  rounded-full font-bold text-center w-3/12 py-1.5">This is Contact Page
+            <h1  className=" text-2xl text-cyan-800  mt-5 ml-5 mr-5 py-1.5 bg-rose-200 rounded-full font-bold text-center">This is Contact Page
             </h1>
             <p className=" mt-3 ml-5 text-base text-cyan-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id soluta neque,sunt deserunt facere laudantium veniam quasi natus fugit ipsam inventore nemo Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. and my companion is this for you of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
 
